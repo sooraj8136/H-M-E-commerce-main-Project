2d3b37a46b69b31a9f828fe901eeab5e3e76707c
Soorajcp <168804842+Soorajcp@users.noreply.github.com>
2024-12-30 22:49:06 -0800

    index.js updated

--- server/index.js
+++ server/index.js
@@ -13,8 +13,8 @@ app.use(express.json())
 app.use(cors({ origin: "http://localhost:5173", credentials: true, methods: [ "GET", "POST", "PUT", "DELETE" ] }));
 app.use(cookieParser())
 
-app.get("/", (req, res, next) => {
-    res.json("hello world");
+app.get("/", (req, res) => {
+    res.send("Hello, Vercel!");
 });
 
 app.use("/api", apiRouter)
@@ -30,3 +30,5 @@ app.listen(process.env.PORT, (err) => {
         console.log(`server starts on port ${process.env.PORT}`)
     }
 })  
+
+module.exports = app;
