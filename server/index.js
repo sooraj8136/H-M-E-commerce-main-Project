14f41191259068b047ca0e15cc4bf33d8875c9e6
Soorajcp <168804842+Soorajcp@users.noreply.github.com>
2024-12-30 22:29:15 -0800

    server index.js updated

--- server/index.js
+++ server/index.js
@@ -13,6 +13,9 @@ app.use(express.json())
 app.use(cors({ origin: "http://localhost:5173", credentials: true, methods: [ "GET", "POST", "PUT", "DELETE" ] }));
 app.use(cookieParser())
 
+app.get("/", (req, res, next) => {
+    res.json("hello world");
+});
 
 app.use("/api", apiRouter)
 
