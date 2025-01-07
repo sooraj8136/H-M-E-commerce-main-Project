0131ec3b01dd5249fa764c8ce2ebcceda72492f6
Soorajcp <168804842+Soorajcp@users.noreply.github.com>
2025-01-06 17:10:27 -0800

    Review 5 commit updated

--- server/index.js
+++ server/index.js
@@ -13,13 +13,9 @@ app.use(express.json())
 app.use(cors({ origin: "http://localhost:5173", credentials: true, methods: [ "GET", "POST", "PUT", "DELETE" ] }));
 app.use(cookieParser())
 
-module.exports = (req, res) => {
-    if (req.url === "/") {
-      res.status(200).json("hello world");
-    } else {
-      res.status(404).json({ error: "Not Found" });
-    }
-  };
+app.get("/", (req, res, next) => {
+    res.json("hello world");
+});
 
 app.use("/api", apiRouter)
 
