5d23505acbd72282ba1d0fa7c85e397f57e1c902
Soorajcp <168804842+Soorajcp@users.noreply.github.com>
2025-01-07 00:04:06 -0800

    updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures relative paths for assets, useful for deployment
-  build: {
-    outDir: 'dist', // Default output directory; ensure this matches Vercel's expected folder
-    assetsDir: 'assets', // Directory for static assets (e.g., CSS, JS)
-  },
-  server: {
-    port: 4000, // Optional: Specify the local dev server port
-  },
-});
+})
