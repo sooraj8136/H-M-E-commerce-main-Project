dc83b5b1aaebffc95e66673b95c2a1b8ed0a87af
Soorajcp <168804842+Soorajcp@users.noreply.github.com>
2025-01-08 19:29:38 -0800

    index.file updated

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: ["http://localhost:5173", "https://h-m-e-commerce-main-project-client.vercel.app"],
         credentials: true, 
         methods: ["GET", "POST", "PUT", "DELETE"] 
     })
