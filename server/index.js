bf63e831d6c68c82373bfee4f64a5e9c53451cd6
Soorajcp <168804842+Soorajcp@users.noreply.github.com>
2025-01-08 18:02:28 -0800

    CORS error

--- server/index.js
+++ server/index.js
@@ -10,7 +10,15 @@ const app = express()
 connectDB()
 
 app.use(express.json())
-app.use(cors({ origin: "http://localhost:5173", credentials: true, methods: [ "GET", "POST", "PUT", "DELETE" ] }));
+
+app.use(
+    cors({
+        origin: ["http://localhost:5173", "https://h-m-e-commerce-main-project-client.vercel.app"], 
+        credentials: true, 
+        methods: ["GET", "POST", "PUT", "DELETE"]
+    })
+);
+
 app.use(cookieParser())
 
 app.get("/", (req, res, next) => {
