3c9ce23bca38e011ec5c8514cd40039d978da28e
Soorajcp <168804842+Soorajcp@users.noreply.github.com>
2025-01-08 18:36:18 -0800

    PORT updated successfully

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 
 app.use(
     cors({
-        origin: "http://localhost:5174", 
+        origin: "http://localhost:5173", 
         credentials: true, 
         methods: ["GET", "POST", "PUT", "DELETE"]
     })
