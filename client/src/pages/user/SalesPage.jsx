8ee014747ecdd64da10a6c98b1c85722e68965ea
Soorajcp <168804842+Soorajcp@users.noreply.github.com>
2025-02-24 23:15:47 -0800

    UI updated

--- client/src/pages/user/SalesPage.jsx
+++ client/src/pages/user/SalesPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Container } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
+import HomeWidget from '../../components/user/HomeWidget';
 
 function SalesPage() {
 
@@ -15,15 +16,15 @@ function SalesPage() {
                         fontWeight: "700",
                     }}>Sale</span></p>
                 </div>
-                <div className='sales-text'>
+                <div className='text-center mt-3'>
+                    <h1 className={darkMode ? "text-black" : "text-white "}>H&M Sale</h1>
+                </div>
+                <HomeWidget />
+                <div className='sales-text' >
                     <div className='container mt-4 d-flex justify-content-center align-items-center'>
                         <div className="position-relative"
                             style={{ maxWidth: "930px", width: "100%" }}>
-                            <div className='text-center '>
-                                <h1 className={darkMode ? "text-black" : "text-white "}>H&M Sale</h1>
-                            </div>
-
-                            <p className={darkMode ? "text-black" : "text-white "}>Shop the sale online at H&M and stock up on lots of great deals! Discover latest styles for less from all our departments.</p>
+                            <p className={darkMode ? "text-black" : "text-white"}>Shop the sale online at H&M and stock up on lots of great deals! Discover latest styles for less from all our departments.</p>
 
 
                             <p className={darkMode ? "text-black" : "text-white "}>Find everything from sharp office staples and casual looks to on-trend fashion and jeans in every fit, shape and colour. Discover discounted clothing, shoes, bags, accessories, swimwear, underwear and loungewear. Shop our clothes sale for stylish and functional sportswear.</p>
