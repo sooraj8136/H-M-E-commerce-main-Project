2bcb159f4d65c2d7bd400c86b81b4c94eb77d262
Soorajcp <168804842+Soorajcp@users.noreply.github.com>
2025-04-01 09:53:24 -0800

    Endpoint Updated

--- server/controllers/paymentController.js
+++ server/controllers/paymentController.js
@@ -25,7 +25,7 @@ const CreateCheckoutSession = async (req, res, next) => {
             payment_method_types: ["card"],
             line_items: lineItems,
             mode: "payment",
-            success_url: `${process.env.CLIENT_DOMAIN}/user/orders`,
+            success_url: `${process.env.CLIENT_DOMAIN}/user/payment-success`,
         });
 
         const order = new OrderDb({
