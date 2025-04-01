28a0299b0dc0342930321bb05232dfbf7428e0c1
Soorajcp <168804842+Soorajcp@users.noreply.github.com>
2025-04-01 12:18:17 -0800

    Final Updation

--- server/model/orderModel.js
+++ server/model/orderModel.js
@@ -1,45 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const orderSchema = new mongoose.Schema(
-//   {
-//     userId: {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: "User",
-//       required: true,
-//     },
-//     items: [
-//       {
-//         productId: {
-//           type: mongoose.Schema.Types.ObjectId,
-//           ref: "products",
-//           required: true,
-//         },
-//         name: String,
-//         quantity: Number,
-//         price: Number,
-//       },
-//     ],
-//     totalAmount: {
-//       type: Number,
-//       required: true,
-//       default: 0,
-//     },
-//     orderStatus: {
-//       type: String,
-//       enum: ["processing", "transit", "out-for-delivery", "delivered"],
-//       default: "processing",
-//     },
-//     canUpdate: {
-//       type: Boolean,
-//       default: false,
-//     },
-//   },
-//   { timestamps: true }
-// );
-
-// module.exports = mongoose.model("order", orderSchema);
-
-
 const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema(
@@ -75,18 +33,6 @@ const orderSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
-    stripeSessionId: { 
-      type: String, 
-    },
-    paymentStatus: {
-      type: String,
-      enum: ["paid", "failed"],
-      default: "paid", // Default is 'pending' until payment is completed
-    },
-    paymentIntentId: { 
-      type: String, 
-      required: false, // Optional, can be useful for tracking the payment attempt
-    }
   },
   { timestamps: true }
 );
