04ab2098ada2877175fe1dd5db75a9df272d7c5c
Soorajcp <168804842+Soorajcp@users.noreply.github.com>
2025-04-01 08:55:18 -0800

    updated axiosInstance

--- client/src/config/axiosInstance.jsx
+++ client/src/config/axiosInstance.jsx
@@ -1,32 +1,6 @@
-// import axios from 'axios'
+import axios from 'axios'
 
-// export const axiosInstance = axios.create({
-//     baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
-//     withCredentials: true,
-// })
-
-import axios from "axios";
-
-// Create axios instance
 export const axiosInstance = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
-  withCredentials: true,
-});
-
-// Add authorization header globally
-axiosInstance.interceptors.request.use(
-  (config) => {
-    // Get token from local storage
-    const token = localStorage.getItem("token"); 
-    if (token) {
-      // Set authorization header
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    // Always return the config
-    return config; 
-  },
-  (error) => {
-    // Handle request error
-    return Promise.reject(error);
-  }
-);
+    baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
+    withCredentials: true,
+})
