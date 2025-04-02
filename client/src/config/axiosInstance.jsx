17da11f3b865cadbebd32aa241da87e6a8e7f611
Soorajcp <168804842+Soorajcp@users.noreply.github.com>
2025-04-01 17:19:32 -0800

    .env updated

--- client/src/config/axiosInstance.jsx
+++ client/src/config/axiosInstance.jsx
@@ -3,8 +3,4 @@ import axios from 'axios'
 export const axiosInstance = axios.create({
     baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
     withCredentials: true,
-    headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${localStorage.getItem("token")}` // Ensure token is sent
-    }
 })
