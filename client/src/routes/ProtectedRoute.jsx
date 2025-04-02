0d4527a128efd76571efe6ac9c093e1c005ae8be
Soorajcp <168804842+Soorajcp@users.noreply.github.com>
2025-04-02 11:42:42 -0800

    Updated payment controll

--- client/src/routes/ProtectedRoute.jsx
+++ client/src/routes/ProtectedRoute.jsx
@@ -1,45 +1,21 @@
-// import React from 'react'
-// import { useSelector } from 'react-redux';
-// import { Outlet, useNavigate } from 'react-router-dom'
-
-// function ProtectedRoute() {
-
-//     const { isUserAuth } = useSelector((state) => state.user)
-
-//     const navigate = useNavigate();
-
-//     if (!isUserAuth) {
-//         navigate("/login");
-//         return;
-//     }
-
-//     return (
-//         <Outlet />
-//     )
-// }
-
-// export default ProtectedRoute
-
-import React, { useEffect } from 'react';
+import React from 'react'
 import { useSelector } from 'react-redux';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom'
 
 function ProtectedRoute() {
-    const { isUserAuth } = useSelector((state) => state.user);
+
+    const { isUserAuth } = useSelector((state) => state.user)
+
     const navigate = useNavigate();
 
-    useEffect(() => {
-        if (!isUserAuth) {
-            navigate('/login');
-        }
-    }, [isUserAuth, navigate]); // useEffect will run when `isUserAuth` changes
-
-    // Prevent rendering the protected component while redirecting
     if (!isUserAuth) {
-        return null;
+        navigate("/login");
+        return;
     }
 
-    return <Outlet />;
+    return (
+        <Outlet />
+    )
 }
 
-export default ProtectedRoute;
+export default ProtectedRoute
