c052ead6562883a87e75bed7cabf7d73c48a1b3b
Soorajcp <168804842+Soorajcp@users.noreply.github.com>
2025-04-01 16:58:42 -0800

    Updated Config file

--- client/src/config/axiosInstance.jsx
+++ client/src/config/axiosInstance.jsx
@@ -1,6 +1,15 @@
-import axios from 'axios'
+// import axios from 'axios'
+
+// export const axiosInstance = axios.create({
+//     baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
+//     withCredentials: true,
+// })
+
+import axios from 'axios';
+
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
 
 export const axiosInstance = axios.create({
-    baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
-    withCredentials: true,
-})
+    baseURL: `${BASE_URL}/api/v1`, // Ensure this matches your backend API structure
+    withCredentials: true, // Enables cookies & authentication headers
+});
