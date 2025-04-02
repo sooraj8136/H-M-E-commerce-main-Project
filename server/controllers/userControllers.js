9acb6088073b4346e198dc84358896c0b2175052
Soorajcp <168804842+Soorajcp@users.noreply.github.com>
2025-04-01 20:36:32 -0800

    updated cookies settings

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -77,10 +77,15 @@ const login = async (req, res) => {
         const token = generateToken(user, "user");
         console.log(token, "=======token")
 
+        // res.cookie("token", token, {
+        //     sameSite: NODE_ENV === "production" ? "None" : "Lax",
+        //     secure: NODE_ENV === "production",
+        //     httpOnly: NODE_ENV === "production"
+        // });
         res.cookie("token", token, {
-            sameSite: NODE_ENV === "production" ? "None" : "Lax",
-            secure: NODE_ENV === "production",
-            httpOnly: NODE_ENV === "production"
+            sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+            secure: process.env.NODE_ENV === "production",
+            httpOnly: process.env.NODE_ENV === "production"
         });
 
         {
