f4636bcdb5446c66773552b35f848389bae830fe
Soorajcp <168804842+Soorajcp@users.noreply.github.com>
2025-04-11 16:19:33 -0800

    payment sec updated

--- client/src/pages/user/PaymentSuccess.jsx
+++ client/src/pages/user/PaymentSuccess.jsx
@@ -1,13 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { axiosInstance } from '../../config/axiosInstance';
 import toast from 'react-hot-toast';
 import { useSelector } from 'react-redux';
 import { LiaThumbsUpSolid } from 'react-icons/lia';
+import { Spinner } from 'react-bootstrap';
 
 const PaymentSuccess = () => {
     const { darkMode } = useSelector((state) => state.mode);
     const navigate = useNavigate();
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const handlePostPayment = async () => {
@@ -16,19 +18,22 @@ const PaymentSuccess = () => {
                     method: "DELETE",
                     url: "/cart/clear-cart"
                 });
-                toast.success('Your payment successful');
+
+                toast.success('Your payment was successful');
 
                 const response = await axiosInstance({
                     method: "POST",
                     url: "/orders/update-stock"
                 });
-                if (response.status === 200) {
-                } else {
+
+                if (response.status !== 200) {
                     toast.error('Failed to update stock.');
                 }
             } catch (error) {
                 toast.error('Something went wrong');
                 console.error(error);
+            } finally {
+                setLoading(false);
             }
         };
 
@@ -41,32 +46,40 @@ const PaymentSuccess = () => {
 
     return (
         <div
-            className={`text-center d-flex flex-column align-items-center justify-content-center ${darkMode ? 'text-black' : 'text-white'}`} >
-            <div className="text-center d-flex flex-column align-items-center justify-content-center" style={{marginTop:"120px"}}>
-                <p style={{ fontWeight: '700', fontSize: 'x-large', }}>THANK YOU FOR PURCHASE</p>
-
-                <div className="d-flex justify-content-center my-3">
-                    <LiaThumbsUpSolid
-                        size={50}
-                        style={{ color: darkMode ? '#000' : '#fff' }}
-                    />
-                </div>
-
-                <button
-                    className="signup-first-btn my-1 w-90"
-                    style={{
-                        maxWidth: '400px',
-                        width: '90%',
-                        fontWeight: '700',
-                        backgroundColor: 'black',
-                        color: 'white',
-                        border: '1px solid white',
-                    }}
-                    onClick={handleDoneClick}
-                >
-                    Done
-                </button>
+            className={`text-center d-flex flex-column align-items-center justify-content-center ${darkMode ? 'text-black' : 'text-white'}`}
+        >
+            {loading ? (
+                <div className="d-flex justify-content-center align-items-center" style={{ marginTop: "180px" }}>
+                <Spinner animation="border" variant={darkMode ? "dark" : "light"} />
+                <span className={`ms-3 ${darkMode ? "text-black" : "text-white"}`}>PROCESSING PAYMENT...</span>
             </div>
+            ) : (
+                <div className="text-center d-flex flex-column align-items-center justify-content-center" style={{ marginTop: "120px" }}>
+                    <p style={{ fontWeight: '700', fontSize: 'x-large' }}>THANK YOU FOR PURCHASE</p>
+
+                    <div className="d-flex justify-content-center my-3">
+                        <LiaThumbsUpSolid
+                            size={50}
+                            style={{ color: darkMode ? '#000' : '#fff' }}
+                        />
+                    </div>
+
+                    <button
+                        className="signup-first-btn my-1 w-90"
+                        style={{
+                            maxWidth: '400px',
+                            width: '90%',
+                            fontWeight: '700',
+                            backgroundColor: 'black',
+                            color: 'white',
+                            border: '1px solid white',
+                        }}
+                        onClick={handleDoneClick}
+                    >
+                        Done
+                    </button>
+                </div>
+            )}
         </div>
     );
 };
