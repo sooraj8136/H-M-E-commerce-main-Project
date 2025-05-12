7936d770a3057301f61328e946efdb375e9cac99
Soorajcp <168804842+Soorajcp@users.noreply.github.com>
2025-05-12 13:32:03 -0800

    updated userlayout

--- client/src/layout/UserLayout.jsx
+++ client/src/layout/UserLayout.jsx
@@ -1,58 +1,3 @@
-// import React, { useEffect } from 'react';
-// import { Outlet, useLocation } from 'react-router-dom';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { axiosInstance } from '../config/axiosInstance';
-// import { clearUser, saveUser } from '../redux/features/userSlice';
-// import Header from '../components/user/Header';
-// import Footer from '../components/user/Footer';
-// import UserHeader from '../components/user/UserHeader';
-// import UserFooter from '../components/user/UserFooter';
-
-// function UserLayout() {
-
-//     const { darkMode } = useSelector((state) => state.mode);
-    
-//     useEffect(() => {
-//         document.body.style.background = darkMode ? "white" : "black";
-//     }, [darkMode]);
-    
-//     const location = useLocation();
-
-//     const { isUserAuth } = useSelector((state) => state.user);
-//     const dispatch = useDispatch();
-
-//     const checkUser = async () => {
-//         try {
-//             const response = await axiosInstance({
-//                 method: "GET",
-//                 url: "/user/check-user"
-//             });
-//             console.log("checkUser Response", response?.data);
-//             dispatch(saveUser(response?.data));
-//         } catch (error) {
-//             console.error("User check failed:", error);
-//             dispatch(clearUser());
-//         }
-//     };
-
-//     useEffect(() => {
-//         checkUser();
-//     }, [location.pathname]);
-
-//     console.log("isUserAuth:", isUserAuth);
-
-//     return (
-//         <>
-//             {isUserAuth ? <UserHeader /> : <Header />}
-//             <Outlet />
-//             {isUserAuth ? <UserFooter /> : <Footer />}
-//         </>
-//     );
-// }
-
-// export default UserLayout;
-
-
 import React, { useEffect } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
@@ -64,13 +9,15 @@ import UserHeader from '../components/user/UserHeader';
 import UserFooter from '../components/user/UserFooter';
 
 function UserLayout() {
-    const { darkMode } = useSelector((state) => state.mode);
 
+    const { darkMode } = useSelector((state) => state.mode);
+    
     useEffect(() => {
         document.body.style.background = darkMode ? "white" : "black";
     }, [darkMode]);
-
+    
     const location = useLocation();
+
     const { isUserAuth } = useSelector((state) => state.user);
     const dispatch = useDispatch();
 
@@ -89,10 +36,7 @@ function UserLayout() {
     };
 
     useEffect(() => {
-        const excludeRoutes = ["/signup", "/login"];
-        if (!excludeRoutes.includes(location.pathname)) {
-            checkUser();
-        }
+        checkUser();
     }, [location.pathname]);
 
     console.log("isUserAuth:", isUserAuth);
